810e916f6626412bcc69b51943f74826c33676de
despair0098 <zijunwang324@gmail.com>
2021-11-07 12:06:16 -0800

    Edited some changes

---
+++ Select_Contains.hpp
@@ -0,0 +1,29 @@
+#ifndef SELECT_CONTAINS_HPP
+#define SELECT_CONTAINS_HPP
+
+#include "select.hpp"
+
+using namespace std;
+
+class Select_Contains: public Select
+{
+private:
+    const Spreadsheet* s;
+    string n; 
+    string w;
+public:
+    Select_Contains(const Spreadsheet* sheet, const string& name, string word)
+    {
+        s = sheet;
+	n = name;
+	w = word;
+    }
+    
+    virtual bool select(const Spreadsheet* sheet, int row) const
+    {    
+	int column = sheet->get_column_by_name(n);
+	return (sheet->cell_data(row, column).find(w));
+    }
+};
+
+#endif 
